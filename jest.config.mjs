54036e8c3074606da3f1130c286b228ebac78a30
devjosecagnini <jose.cagnini@proton.me>
2025-07-29 22:22:54 -0300

    config test with jest ts-jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,14 @@
-import { createDefaultPreset } from "ts-jest";
+import { createDefaultPreset } from 'ts-jest';
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-const testEnvironment = "node";
+const testEnvironment = 'node';
 const transform = {
 	...tsJestTransformCfg,
 };
 
 export default {
 	testEnvironment,
-	transform
-}
+	transform,
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,12 @@
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
+import mod from './mod';
 
 describe('test', () => {
-  test('tests jest', () => {
-    expect('jest').toBe('jest');
-  });
+	test('tests jest', () => {
+		expect('jest').toBe('jest');
+	});
+
+	test('1 + 1', () => {
+		expect(mod.sum(1, 1)).toBe(2);
+	});
 });
